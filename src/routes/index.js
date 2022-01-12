01c5e127f534d25c84bb9be0e57d342eaedf8e2f
vcrolack <victorolack@gmail.com>
2022-01-11 21:06:55 -0300

    Petición para obtener lista de razas y mostrarlas en su vista avanzado.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
+
 
 //HOME
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {'index': true})
 });
 
 //TODAS LAS RAZAS DE PERROS
 router.get('/perros', (req, res) => {
-  res.render('all_dogs')
+  const urlAPI = 'https://dog.ceo/api/breeds/list/all';
+  axios.get(urlAPI)
+  .then(function (response) {
+    let all_dogs = response.data.message;
+    let only_dogs = Object.keys(all_dogs);
+    res.render('all_dogs', {'perros': true, 'dogs': only_dogs})
+  })
+
 });
 
 module.exports = router;
